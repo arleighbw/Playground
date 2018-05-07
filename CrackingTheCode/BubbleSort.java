ea1c8e6a8c7dfc64fbffbcc2f7454ae268f27222
Blayne Wilson <arleighbw@gmail.com>
2018-05-06 20:39:03 -0500

    Update and rename bubbleSort.java to BubbleSort.java
    uppercase class name

--- CrackingTheCode/BubbleSort.java
+++ CrackingTheCode/BubbleSort.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class bubbleSort {
+public class BubbleSort {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
