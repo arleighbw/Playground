3196c4b031968cbc18cf11e79bb040c438205ffd
Blayne Wilson <arleighbw@gmail.com>
2019-04-04 23:22:54 -0700

    Reorganized Renegade.Collections code into DotNetCore folder

---
+++ DotNetCore/Renegade.Collections/Renegade.Collections.Tests/NodeTests.cs
@@ -0,0 +1,78 @@
+using System;
+using Xunit;
+
+namespace Renegade.Collections.Tests
+{
+    public class NodeStringTests
+    {
+        [Fact]
+        public void SameString_NodeEquals_True()
+        {
+            Node<string> node = new Node<string>("blayne");
+            Node<string> node2 = new Node<string>("blayne");
+
+            var actual = node.Equals(node2);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void DiffString_NodeEquals_False()
+        {
+            Node<string> node = new Node<string>("blayne");
+            Node<string> node2 = new Node<string>("wilson");
+
+            var actual = node.Equals(node2);
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void null_NodeEquals_False()
+        {
+            Node<string> node = new Node<string>("blayne");
+            Node<string> node2 = null;
+
+            var actual = node.Equals(node2);
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void emptyString_NodeEquals_False()
+        {
+            Node<string> node = new Node<string>("blayne");
+            Node<string> node2 = new Node<string>(string.Empty);
+
+            var actual = node.Equals(node2);
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void SameNode_NodeEqualEqual_False()
+        {
+            Node<string> node = new Node<string>("blayne");
+            Node<string> node2 = new Node<string>("blayne");
+
+            var actual = (node == node2);
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void SameNodeRef_RefNodeEqualEqualNode_True()
+        {
+            Node<string> node = new Node<string>("blayne");
+            Node<string> root = node;
+
+            var actual = (node == root);
+
+            Assert.True(actual);
+        }
+
+        // check .GetHashCode();
+
+        // check .ToString()
+    }
+}
